8a97e074b5c14a5ac46384e9afe8a2a819cb5a46
fahad <fjkiani@icloud.com>
2019-07-07 13:40:15 +0500

    changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,54 @@
 
-// Mobile Navbar
+
+
+// $(document).ready(function() {
+
+
+// /* Scroll on buttons */
+    
+//     /* Scroll on buttons */
+//     $('.js--scroll-to-plans').click(function () {
+// 		$('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+// 	 });
+	 
+// 	 $('.js--scroll-to-start').click(function () {
+// 		$('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+// 	 });
+
+// 	});
+
+
+
+// No autoplay on carousel ///
+
+// $('.carousel').carousel({
+//     data-interval: false;
+// }); 
+
+
+function openCity(evt, cityName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
+
+
+
+/* Mobile Navbar */
 
 
 const navSlide = () => {
+
+
+
 	window.onload = navSlide;
 
 	const burger = document.querySelector('.burger');
@@ -35,12 +81,19 @@ const navSlide = () => {
 navSlide();
 
 
-
 function openNav() {
   document.getElementById("navLinks").style.width = "100%";
 }
 
+
 function closeNav() {
   document.getElementById("navLinks").style.width = "0%";
-}
+	}
 
+
+
+	$(document).on('click','.navbar-collapse.in',function(e) {
+		if( $(e.target).is('a') ) {
+			$(this).collapse('hide');
+		}
+	});
