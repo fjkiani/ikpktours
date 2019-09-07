7d90f78e2dd821e57d135d53cbc251eb2d2800ca
fahad <fjkiani@icloud.com>
2019-09-06 20:31:06 -0400

    changes

--- js/script2.js
+++ js/script2.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
   
-	//   hover effects 
-		$("h2").hover(function(){
-	$(this).css("background-color", "yellow");
-	}, function(){
-	$(this).css("color", "black");
-	
-	});
-	
-	
-	/* Animations on scroll */
+
+		/* Animations on scroll */
 	$('.js--wp-1').waypoint(function(direction) {
 	$('.js--wp-1').addClass('animated pulse');
+	console.log("Hi");
 	}, {
-	offset: '100%'
+	offset: '50%'
 	});
 	
 	
@@ -22,13 +15,13 @@ $(document).ready(function() {
 	$('.js--wp-2').addClass('animated fadeIn');
 	console.log("offset wp2");
 	}, {
-	offset: '60%'
+	offset: '50%'
 	});
 	
 	$('.js--wp-3').waypoint(function(direction) {
 	$('.js--wp-3').addClass('animated fadeIn');
 	}, {
-	offset: '80%'
+	offset: '50%'
 	});
 	
 	$('.js--wp-4').waypoint(function(direction) {
@@ -40,57 +33,65 @@ $(document).ready(function() {
 	$('.js--wp-5').waypoint(function(direction) {
 	$('.js--wp-5').addClass('animated fadeIn');
 	}, {
-	offset: '80%'
+	offset: '50%'
 	});
 	
 	$('.js--wp-6').waypoint(function(direction) {
 	$('.js--wp-6').addClass('animated fadeInLeftBig');
 	}, {
-	offset: '100%'
+	offset: '50%'
 	});
 	
 	$('.js--wp-7').waypoint(function(direction) {
 	$('.js--wp-7').addClass('animated fadeIn');
 	}, {
-	offset: '100%'
+	offset: '50%'
 	});
 	
 	$('.js--wp-7').waypoint(function(direction) {
 	$('.js--wp-7').addClass('animated fadeIn');
 	}, {
-	offset: '100%'
+	offset: '50%'
 	});
 	
 
 	$('.js--wp-8').waypoint(function(direction) {
 		$('.js--wp-8').addClass('animated fadeIn');
 		}, {
-		offset: '100%'
+		offset: '50%'
 		});
 
 
 	$('.js--wp-9').waypoint(function(direction) {
-		$('.js--wp-9').addClass('animated rollIn');
+		$('.js--wp-9').addClass('animated fadeInLeft');
 		}, {
-		offset: '100%'
+		offset: '50%'
 		});
 
 		$('.js--wp-10').waypoint(function(direction) {
 			$('.js--wp-10').addClass('animated fadeIn');
 			}, {
-			offset: '100%'
+			offset: '50%'
 			});	
 
 			$('.js--wp-11').waypoint(function(direction) {
 				$('.js--wp-11').addClass('animated fadeIn');
 				}, {
-				offset: '100%'
+				offset: '50%'
 				});	
 
 				$('.js--wp-12').waypoint(function(direction) {
 					$('.js--wp-12').addClass('animated fadeIn');
 					}, {
-					offset: '100%'
+					offset: '50%'
 					});		
+
+		//   hover effects 
+		$("h2").hover(function(){
+			$(this).css("background-color", "yellow");
+			}, function(){
+			$(this).css("color", "black");
+			
+			});				
 	
 	});
